9ebf5c6a51d79a7ad53ae089f042db54653483b7
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-11-24 18:08:05 -0800

    useLocalDependencies = true

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import Foundation
 private let name = "SPFKAudioBase" // Swift target
 private let dependencyNames: [String] = ["SPFKBase", "SPFKTesting"]
 private let dependencyBranch = "main"
-private let useLocalDependencies: Bool = false
+private let useLocalDependencies: Bool = true
 private let platforms: [PackageDescription.SupportedPlatform]? = [
     .macOS(.v12),
     .iOS(.v15),
