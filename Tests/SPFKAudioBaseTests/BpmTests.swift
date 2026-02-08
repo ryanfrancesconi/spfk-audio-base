d5788c43a2f7d6962cfaf11d43c4f7319e4e9c86
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-02-07 17:23:48 -0800

    create spfk-tempo for swift wrapper to BPM functions

--- Tests/SPFKAudioBaseTests/BpmTests.swift
+++ Tests/SPFKAudioBaseTests/BpmTests.swift
@@ -6,8 +6,8 @@ import SPFKTesting
 import Testing
 
 final class BpmTests {
-    @Test func isMultiple() {
-        let _80 = Bpm(80)
+    @Test func isMultiple() async throws {
+        let _80 = try Bpm(80)
         #expect(_80.isMultiple(of: 20))
         #expect(_80.isMultiple(of: 40))
         #expect(_80.isMultiple(of: 80))
