f88a46d15d5b5e0f1b95ee14aee4e609047b507a
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-05-12 16:09:15 -0700

    Update dependency version requirements to match 1.x tags

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "0.0.3"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.9"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "1.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "1.0.1"),
     ],
     targets: [
         .target(
