42dcaa2e6f17b328dac961bdfc2d7b54cbfb86ec
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-06-01 14:14:30 -0700

    cleaned invalid test out

--- Tests/SPFKAudioBaseTests/AudioDefaultsTests.swift
+++ Tests/SPFKAudioBaseTests/AudioDefaultsTests.swift
@@ -6,8 +6,6 @@ import SPFKBase
 import SPFKTesting
 import Testing
 
-// Note, more tests in SPFKMetadataTests
-
 @Suite(.serialized, .tags(.file))
 class AudioDefaultsTests {
     @Test func setters() async throws {
@@ -24,40 +22,9 @@ class AudioDefaultsTests {
         await AudioDefaults.shared.update(minimumSampleRateSupported: 11025)
         await AudioDefaults.shared.update(systemFormat: AVAudioFormat(standardFormatWithSampleRate: 11025, channels: 2)!)
         #expect(await AudioDefaults.shared.sampleRate == 11025)
-        
+
         await AudioDefaults.shared.update(enforceMinimumSampleRate: true)
         await AudioDefaults.shared.update(systemFormat: AVAudioFormat(standardFormatWithSampleRate: AudioDefaults.defaultFormat.sampleRate, channels: 2)!)
         #expect(await AudioDefaults.shared.sampleRate == 48000)
-
-    }
-    
-    // just watching how it plays outF
-    @Test func sharedState() async throws {
-        let task1 = Task {
-            for _ in 0 ... 10 {
-                Log.debug("🟡 enter 11k")
-                await AudioDefaults.shared.update(enforceMinimumSampleRate: false)
-                await AudioDefaults.shared.update(minimumSampleRateSupported: 11025)
-                await AudioDefaults.shared.update(systemFormat: AVAudioFormat(standardFormatWithSampleRate: 11025, channels: 2)!)
-                #expect(await AudioDefaults.shared.sampleRate == 11025)
-                Log.debug("🟡 set 11k")
-
-            }
-        }
-        
-        let task2 = Task {
-            for _ in 0 ... 10 {
-                Log.debug("🟠 enter 48k")
-                await AudioDefaults.shared.update(enforceMinimumSampleRate: true)
-                await AudioDefaults.shared.update(systemFormat: AVAudioFormat(standardFormatWithSampleRate: AudioDefaults.defaultFormat.sampleRate, channels: 2)!)
-                #expect(await AudioDefaults.shared.sampleRate == 48000)
-                Log.debug("🟠 set 48k")
-            }
-        }
-        
-        _ = await task1.result
-        _ = await task2.result
-        
-        Log.debug(await AudioDefaults.shared.sampleRate)
     }
 }
