f328d6f3ae54250546a743057545de504c4fe2d0
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-06-08 10:01:44 -0700

    Update kAudioUnitManufacturer_Spongefork to SpFk to satisfy auval case requirement

--- Sources/SPFKAudioBase/Definitions/Constants.swift
+++ Sources/SPFKAudioBase/Definitions/Constants.swift
@@ -1,5 +1,5 @@
 import Foundation
 import SPFKBase
 
-public let kAudioUnitManufacturer_Spongefork: FourCharCode = "spfk".fourCC ?? 0
+public let kAudioUnitManufacturer_Spongefork: FourCharCode = "SpFk".fourCC ?? 0
 public let kAudioUnitManufacturer_AudioKit: FourCharCode = "AuKt".fourCC ?? 0
